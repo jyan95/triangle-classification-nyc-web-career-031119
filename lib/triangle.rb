5c9e596974b6b827cd4699d8ad95fbabe1d83466
github username <github email address>
2019-03-19 17:31:16 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,6 +2,17 @@ class Triangle
   attr_accessor :side1, :side2, :side3, :equilateral,:isosceles,:scalene
 
   def initialize(s1,s2,s3)
+    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
+      raise TriangleError
+    end
+
+    if s1 <= 0 || s2 <= 0 ||s3 <= 0
+      raise TriangleError
+    end
+
+    if s1 == nil || s2 == nil ||s3 == nil
+      raise TriangleError
+    end
     @s1 = s1
     @s2 = s2
     @s3 = s3
@@ -33,10 +44,13 @@ class Triangle
         :scalene
       end
     else
-      raise TriangleError
+      raise Triangle
     end
   end
 end
 
 class TriangleError < StandardError
+  def message
+    "This is not a valid triangle."
+  end
 end
